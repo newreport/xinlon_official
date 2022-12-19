85998e974fd2161b656c2c098072c97c3afe9506
newreport <haruki959@qq.com>
2022-12-20 01:14:55 +0800

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", //指定静态资源存放路径
+  }
 })
